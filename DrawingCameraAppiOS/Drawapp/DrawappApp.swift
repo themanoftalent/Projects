ba1c254ef28f5113524e8751dec5d6bac37b9197
Mehmet Akif Cifci <17935050+themanoftalent@users.noreply.github.com>
2024-04-15 10:28:07 +0300

    umuterguler

---
+++ DrawingCameraAppiOS/Drawapp/DrawappApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawappApp.swift
+//  Drawapp
+//
+//  Created by Umut on 10.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct DrawappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
